4c03a5976901340a05930313a468e5cdc91e3242
neharaut00 <neharaut372@gmail.com>
2022-11-30 09:28:27 +0530

    bug resolved

--- healtech/src/components/HeroSection.js
+++ healtech/src/components/HeroSection.js
@@ -1,21 +1,26 @@
 import React from "react";
 import robo from "../assets/robo.jpg";
-import { about } from "../constants"
+import { about } from "../constants";
 
 function HeroSection() {
   return (
     <div className="relative h-[400px] xs:h-[550px] w-full mx-8 mt-10 mb-0 sm:mx-10 md:mx-24 md:mb-56 md:mt-5">
-      <div id="about" className="absolute top-[0px] md:top-[50px] lg:top-[90px] z-[3] mx-8 -left-10 md:-left-20 lg:left-0 ">
+      <div
+        id="about"
+        className="absolute top-[0px] md:top-[50px] lg:top-[90px] z-[3] mx-8 -left-10 md:-left-20 lg:left-0 "
+      >
         <h1 className="text-primary_font font-extrabold inline font-inter text-[60px] lg:text-[120px] sm:text-[100px] leading-none">
           Digitalising <br />
           <span className="">Healthcare</span>
         </h1>
         <h3 className="font-inter text-primary_font opacity-60 block my-2 md:my-6 text-[20px] sm:text-[30px] w-[80%] md:w-[60%]">
-          { about }
+          {about}
         </h3>
-        <button className="btn-yellow inline-block w-fit h-fit py-4 px-8 md:py-6 md:px-10 text-[16px] md:text-[20px] my-3 lg:my-14">
-          Contact Us
-        </button>
+        <a href="#contact">
+          <button className="btn-yellow inline-block w-fit h-fit py-4 px-8 md:py-6 md:px-10 text-[16px] md:text-[20px] my-3 lg:my-14">
+            Contact Us
+          </button>
+        </a>
         <button className="btn-white inline w-fit h-fit py-4 px-8 md:py-6 md:px-10 text-[16px] md:text-[20px] my-3 lg:my-14">
           Scroll{" "}
           <svg
@@ -41,8 +46,8 @@ function HeroSection() {
       </div>
       <div className="absolute top-[0px] md:top-[50px] lg:top-[90px] z[-1] mx-8 -left-10 md:-left-20 lg:left-0 ">
         <h1 className="text-primary_font font-extrabold font-inter text-[60px] lg:text-[120px] sm:text-[100px] leading-none">
-        Digitalising <br />
-           <span className="">Healthcare</span>
+          Digitalising <br />
+          <span className="">Healthcare</span>
         </h1>
       </div>
       <img
